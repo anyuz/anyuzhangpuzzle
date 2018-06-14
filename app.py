9d9eac71cc0828427837aade963c4e9c7ce3cfad
anyuz <anyuz@smu.edu>
2018-06-14 11:17:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import datetime
 import os
 from flask import Flask, render_template, redirect, url_for
-from forms import ItemForm
+from forms import ItemForm, QueryForm
 from models import Items
 from database import db_session
 
@@ -28,7 +28,36 @@ def success():
     results = qry.all()
 
     return str(results)
-  
+@app.route("/delete", methods=('GET', 'POST'))
+def delete_item():
+    form = QueryForm()
+    if form.validate_on_submit():
+        db_session.query(Items).filter_by(name = form.name.data).delete(synchronize_session=False)
+        db_session.commit()
+        return redirect(url_for('success'))
+    return render_template('delete.html', form=form)
+
+@app.route("/query", methods=('GET', 'POST'))
+def query_item():
+    form = QueryForm()
+    if form.validate_on_submit():
+        results = db_session.query(Items).filter_by(name = form.name.data).all()
+        return str(results)
+    return render_template('query.html',form=form)
+
+@app.route("/items/<item>",methods=('GET',))
+def disp_item(item):
+    results = db_session.query(Items).filter_by(name=item).all()
+    if results:
+        return str(results)
+    else:
+        return "404 page not found"
+
+@app.route("/allitems",methods=('GET',))
+def disp_all():
+    qry = db_session.query(Items)
+    results = qry.all()
+    return str(results)
 
 if __name__ == '__main__':
     #app.run(host='0.0.0.0')
